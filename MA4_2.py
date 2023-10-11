c3e27d172a8a017a06e6e317afbb46d1fd5b19b0
Miniskift <147612911+Miniskift@users.noreply.github.com>
2023-10-11 15:47:08 +0200

    upp

--- MA4_2.py
+++ MA4_2.py
@@ -2,11 +2,76 @@
 
 from person import Person
 
+import random
+import math
+import matplotlib.pyplot as plt
+from time import perf_counter as pc
+from time import sleep as pause
+import concurrent.futures as future
+import numpy as np
+from numba import njit
+
+@njit
+
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return(fib_py(n-1) + fib_py(n-2))
+	
+@njit
+def fib_numba(n):
+	if n <= 1:
+		return n
+	else:
+		return fib_numba(n-1) + fib_numba(n-2)
+
+def graph(q,w):
+	x = range(q,w)
+	y_cpp = []
+	y_numba = []
+	y_py = []
+
+	for i in x:
+		t1 = pc()
+		f = Person(i)
+		f.fib()
+		t2 = pc()
+		fib_numba(i)
+		t3 = pc()
+		fib_py(i)
+		t4 = pc()
+
+		y_cpp.append(t2-t1)
+		y_numba.append(t3-t2)
+		y_py.append(t4-t3)
+
+	plt.plot(x, y_cpp, "r", label = "C++")
+	plt.plot(x, y_numba, "g", label = "Numba")
+	plt.plot(x, y_py, "b", label = "py")
+
+	plt.yscale('log')
+	plt.xlabel('n')
+	plt.ylabel('Time (s)')
+	plt.title('r = C++, g = Numba, B = Py')
+	plt.savefig(f'{q} to {w} logFib_TimeComparsion.png')
+
+def time47():
+	x = 47
+	t1 = pc()
+	f = Person(x)
+	print(f'C++: {f.fib()}')
+	t2 = pc()
+	print(f'numba: {fib_numba(x)}')
+	t3 = pc()
+
+	print(f'C++: {round(t2-t1,4)}')
+	print(f'Numba:{round(t3-t2,4)}')
+
 def main():
-	f = Person(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
+	graph(20,30)
+	time47()
+	graph(30,45)
 
 if __name__ == '__main__':
 	main()
